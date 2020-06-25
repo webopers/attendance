0b1c875f7417967f31e83041d977645a88d4a560
Nguyen Khac Khanh <encacap@gmail.com>
2020-06-25 18:02:54 +0700

    add isPhone rule for Validator lib

--- assets/js/lib/validator.js
+++ assets/js/lib/validator.js
@@ -1,91 +1,100 @@
 function Validator(options) {
-	const formElement = document.querySelector(options.form);
-	const selectorRules = {};
+  const formElement = document.querySelector(options.form);
+  const selectorRules = {};
 
-	const validate = (inputElement, rules) => {
-		const errorElement = inputElement.parentElement.querySelector(options.errorSelector);
-		let errorMessage;
+  const validate = (inputElement, rules) => {
+    const errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+    let errorMessage;
 
-		selectorRules[rules.selector].forEach((rule) => {
-			if (!errorMessage) errorMessage = rule(inputElement.value);
-		});
+    selectorRules[rules.selector].forEach((rule) => {
+      if (!errorMessage) errorMessage = rule(inputElement.value);
+    });
 
-		if (errorMessage) {
-			inputElement.parentElement.classList.add("invalid");
-			errorElement.innerText = errorMessage;
-		} else {
-			inputElement.parentElement.classList.remove("invalid");
-			errorElement.innerText = "";
-		}
-		return !errorMessage;
-	};
+    if (errorMessage) {
+      inputElement.parentElement.classList.add("invalid");
+      errorElement.innerText = errorMessage;
+    } else {
+      inputElement.parentElement.classList.remove("invalid");
+      errorElement.innerText = "";
+    }
+    return !errorMessage;
+  };
 
-	if (formElement) {
-		formElement.onsubmit = (event) => {
-			let isFormValid = true;
-			event.preventDefault();
-			options.rules.forEach((rule) => {
-				const inputElement = formElement.querySelector(rule.selector);
-				const isValid = validate(inputElement, rule);
-				if (!isValid) isFormValid = false;
-			});
-			if (isFormValid) {
-				const enableInputs = formElement.querySelectorAll("[name]:not([disabled])");
-				const formValues = Array.from(enableInputs).reduce((values, input) => {
-					// eslint-disable-next-line no-return-assign
-					return (values[input.name] = input.value) && values;
-				}, {});
-				if (typeof options.onSubmit === "function") {
-					options.onSubmit(formValues);
-				} else {
-					formElement.submit();
-				}
-			}
-		};
+  if (formElement) {
+    formElement.onsubmit = (event) => {
+      let isFormValid = true;
+      event.preventDefault();
+      options.rules.forEach((rule) => {
+        const inputElement = formElement.querySelector(rule.selector);
+        const isValid = validate(inputElement, rule);
+        if (!isValid) isFormValid = false;
+      });
+      if (isFormValid) {
+        const enableInputs = formElement.querySelectorAll("[name]:not([disabled])");
+        const formValues = Array.from(enableInputs).reduce((values, input) => {
+          // eslint-disable-next-line no-return-assign
+          return (values[input.name] = input.value) && values;
+        }, {});
+        if (typeof options.onSubmit === "function") {
+          options.onSubmit(formValues);
+        } else {
+          formElement.submit();
+        }
+      }
+    };
 
-		options.rules.forEach((rule) => {
-			const inputElement = formElement.querySelector(rule.selector);
-			const errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+    options.rules.forEach((rule) => {
+      const inputElement = formElement.querySelector(rule.selector);
+      const errorElement = inputElement.parentElement.querySelector(options.errorSelector);
 
-			if (Array.isArray(selectorRules[rule.selector])) {
-				selectorRules[rule.selector].push(rule.test);
-			} else {
-				selectorRules[rule.selector] = [rule.test];
-			}
+      if (Array.isArray(selectorRules[rule.selector])) {
+        selectorRules[rule.selector].push(rule.test);
+      } else {
+        selectorRules[rule.selector] = [rule.test];
+      }
 
-			if (inputElement) {
-				inputElement.onblur = () => validate(inputElement, rule);
-				inputElement.oninput = () => {
-					inputElement.parentElement.classList.remove("invalid");
-					errorElement.innerText = "";
-				};
-			}
-		});
-	}
+      if (inputElement) {
+        inputElement.onblur = () => validate(inputElement, rule);
+        inputElement.oninput = () => {
+          inputElement.parentElement.classList.remove("invalid");
+          errorElement.innerText = "";
+        };
+      }
+    });
+  }
 }
 
 Validator.isRequired = (selector, message) => ({
-	selector,
-	test: (value) => (value.trim() ? undefined : message || "Vui lòng nhập trường này"),
+  selector,
+  test: (value) => (value.trim() ? undefined : message || "Vui lòng nhập trường này"),
 });
 
 Validator.isEmail = (selector, message) => ({
-	selector,
-	test: (value) => {
-		// eslint-disable-next-line no-useless-escape
-		const regex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-		return regex.test(value) ? undefined : message || "Trường này phải là Email";
-	},
+  selector,
+  test: (value) => {
+    // eslint-disable-next-line no-useless-escape
+    const regex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return regex.test(value) ? undefined : message || "Trường này phải là Email";
+  },
 });
 
 Validator.minLength = (selector, min, message) => ({
-	selector,
-	test: (value) => (value.length >= min ? undefined : message || `Vui lòng nhập tối thiểu ${min} ký tự`),
+  selector,
+  test: (value) => (value.length >= min ? undefined : message || `Vui lòng nhập tối thiểu ${min} ký tự`),
 });
 
 Validator.isConfirmed = (selector, getConfirmValue, message) => ({
-	selector,
-	test: (value) => (value === getConfirmValue() ? undefined : message || "Nhập lại không chính xác"),
+  selector,
+  test: (value) => (value === getConfirmValue() ? undefined : message || "Nhập lại không chính xác"),
+});
+
+Validator.isPhone = (selector, message) => ({
+  selector,
+  test: (value) => {
+    // eslint-disable-next-line no-useless-escape
+    const regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    return regex.test(value) ? undefined : message || "Trường này phải là số điện thoại";
+  },
 });
 
 export { Validator as default };
