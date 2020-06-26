b41fd689cf9002486d4f954786c3b462806c14ee
Nguyen Khac Khanh <encacap@gmail.com>
2020-06-26 08:13:10 +0700

    add getTime function

---
+++ assets/js/lib/time.js
@@ -0,0 +1,13 @@
+const getTime = (type) => {
+  const date = new Date();
+  const today = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+  const month = date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+  const year = date.getFullYear();
+  const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+  const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+  if (type === "miliseconds") return date.getTime();
+};
+
+const formatTime = "";
+
+export { getTime, formatTime };
